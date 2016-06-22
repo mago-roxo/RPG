70004a628d6df243dd4f99470119e6bf68c6a296
Marcos Zlotnik <marcoszlotnik@gmail.com>
2016-06-22 15:31:49 -0300

    Update source.py

--- source.py
+++ source.py
@@ -1,1 +1,13 @@
-
+print "\n\n                           SOULBOUND\n\n"
+print "PRESS 1 TO PLAY\nPRESS 2 TO CREDITS\nPRESS 3 FOR INSTRUCTIONS\nPRESS 4 TO SAVE YOUR SETTINGS"
+menu = input("\n\nENTER: ")
+if menu == 1:
+    print "X"
+elif menu == 2:
+    print "X"
+elif menu == 3:
+    print "X"
+elif menu == 4:
+    print "X"
+else:
+    print "ERROR"
